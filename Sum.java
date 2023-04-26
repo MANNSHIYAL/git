74a20e6660d09f8515d69a2eaa47d370f939b6c8
RINKATOTO <mannshiyal43024@gmail.com>
2023-04-26 18:33:46 +0530

    Modifing Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ class Sum{
     public static void main(String[] args) {
         int a=12;
         int b=10;
-        System.out.println(a+b);
+        System.out.println("Addition of two numbers is " +(a+b));
     }   
 }
